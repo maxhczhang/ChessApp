44dd7b7fbf2f3f137d5c1571dd6e071e2be89fc1
Max Zhang <maxhz1@uci.edu>
2024-11-13 23:41:30 -0800

    final change

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const PORT = 5001;
 
 // backend server storage for games
 const games = {};
-
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // backend api to save game
 app.post('/api/save-game', (req, res) => {
